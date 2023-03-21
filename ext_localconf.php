9ce5e83e07c7b60a5172214bb7fb654529d82e8f
Kacper Janczyk <k.janczyk@macopedia.com>
2023-03-21 10:36:53 +0100

    Unescape unicode while json encoding feed message from facebook - localconf fix

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,2 +1,6 @@
 <?php
 defined('TYPO3') or die('Access denied.');
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\Pixelant\PxaSocialFeed\Feed\Update\FacebookFeedUpdater::class] = [
+    'className' => \FriendsOfTYPO3\HeadlessSocialFeed\XClass\FacebookFeedUpdater::class
+];
