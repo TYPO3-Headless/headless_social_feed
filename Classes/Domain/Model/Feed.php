d52ee7f3ccb0ab9af03fa44bce618b7f5c08a28e
Kacper Janczyk <k.janczyk@macopedia.com>
2023-03-24 13:42:18 +0100

    Change rendered date format

--- Classes/Domain/Model/Feed.php
+++ Classes/Domain/Model/Feed.php
@@ -2,6 +2,9 @@
 
 namespace FriendsOfTYPO3\HeadlessSocialFeed\Domain\Model;
 
+use TYPO3\CMS\Core\Utility\ArrayUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 class Feed extends \Pixelant\PxaSocialFeed\Domain\Model\Feed
 {
     protected int $comments = 0;
@@ -18,5 +21,16 @@ class Feed extends \Pixelant\PxaSocialFeed\Domain\Model\Feed
     {
         $message = parent::getMessage();
         return base64_decode($message);
+    }
+
+    protected array $postDateNew = [];
+
+    public function getPostDateNew(): array
+    {
+        $result = [
+            "date" => date_format($this->getPostDate(), "d.m.Y"),
+        ];
+        ArrayUtility::mergeRecursiveWithOverrule($result, (array)$this->getPostDate()->getTimezone());
+        return $result;
     }
 }
